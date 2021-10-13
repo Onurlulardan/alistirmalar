53a7c4d10415dc83068c398320383d3b04b70ed8
Onur Altuntaş <50622451+Onurlulardan@users.noreply.github.com>
2021-10-13 19:09:16 +0300

    proje-3/final

--- proje-3/script.js
+++ proje-3/script.js
@@ -3,7 +3,7 @@ const prevIcon = '<img src="img/left.svg">';
 
 
 
-$('.owl-carousel').owlCarousel({
+$('.owlfirst .owl-carousel').owlCarousel({
     loop: true,
     nav: true,
     navText: [
@@ -23,3 +23,63 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+
+const owlSeconNextIcon = '<i class="fas fa-arrow-right"></i>';
+const owlSecondPrevIcon = '<i class="fas fa-arrow-left"></i>';
+
+
+$('.owl-carousel').owlCarousel({
+    dots: false,
+    nav: true,
+    loop: true,
+    autoplay: true,
+    items: 1,
+    navText: [
+        owlSecondPrevIcon,
+        owlSeconNextIcon
+    ],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+// modal menu 
+const modalinfoac = document.getElementById("hamburgerbtn");
+const modalinfo = document.querySelector(".modal-menu");
+
+const modalinfokapat = document.getElementById("closebtn");
+
+
+modalinfoac.addEventListener("click", ()=>{
+    modalinfo.style.display = "flex";
+});
+
+modalinfokapat.addEventListener("click", ()=>{
+    modalinfo.style.display = "none";
+
+});
+
+// modal menu end
+// modal randevu menu
+const modalrandevuac = document.getElementById("randevubtn");
+const modalrandevu = document.querySelector(".modal-menu-randevu");
+const modalrandevukapat = document.getElementById("form-modal-closebtn");
+
+
+modalrandevuac.addEventListener("click", ()=>{
+    modalrandevu.style.display = "flex";
+});
+
+modalrandevukapat.addEventListener("click", ()=>{
+    modalrandevu.style.display = "none";
+});
+// modal randevu menu end
