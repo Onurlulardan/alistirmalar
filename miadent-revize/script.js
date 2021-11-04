87a49444889153d3d7f27efcf08e46ba6733e5d7
Onur Altuntaş <50622451+Onurlulardan@users.noreply.github.com>
2021-11-04 19:20:57 +0300

    miadentrevize-5

--- miadent-revize/script.js
+++ miadent-revize/script.js
@@ -1,6 +1,7 @@
 var swiper = new Swiper(".mySwiper", {
     loop: true,
     cssMode: true,
+    clickable: true,
     navigation: {
       nextEl: ".swiper-ileri",
       prevEl: ".swiper-geri",
@@ -11,3 +12,55 @@ var swiper = new Swiper(".mySwiper", {
     mousewheel: true,
     keyboard: true,
   });
+ // second swiper
+ var swiper = new Swiper(".mySecondSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  cssMode: true,
+  clickable: true,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-second-next',
+    prevEl: '.swiper-second-prev',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  }
+});
+// third swiper
+var swiper = new Swiper(".myThirdSwiper", {
+  slidesPerView: 2,
+  loop: true,
+  cssMode: true,
+  clickable: true,
+  pagination: {
+    el: ".third-Slider-peg",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+  },
+  mousewheel: true,
+  keyboard: true,
+});
