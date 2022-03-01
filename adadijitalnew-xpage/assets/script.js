f0453af072fdbde894f179320f7d3f70462a96d1
Onur Altuntaş <50622451+Onurlulardan@users.noreply.github.com>
2022-03-01 13:18:39 +0300

    Add files via upload

--- adadijitalnew-xpage/assets/script.js
+++ adadijitalnew-xpage/assets/script.js
@@ -1,20 +1,79 @@
-const swiper = new Swiper('.firstSwiper', {
+// const swiper = new Swiper('.firstSwiper', {
+//     slidesPerView: 1,
+//     loop: true,
+//     mousewheel: {
+//         invert: true,
+//     },
+//     pagination: {
+//         el: '.first-peg',
+//         clickable:true,
+//     },
+//     breakpoints: {
+//         450: {
+//         },
+//      },
+// });
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
+
+var swiperf = new Swiper('.firstSwiper', {
     slidesPerView: 1,
-    direction: 'horizontal',
+    centeredSlides: true,
+    pagination: {
+        el: ".albumSlide .swiper-pagination",
+            clickable:"true"
+    },
+    navigation: {
+        nextEl: ".albumSlide .swiper-button-next",
+        prevEl: ".albumSlide .swiper-button-prev"
+    },
+    effect: "flipEffect",
+    slidesOffsetBefore: 0,
+    speed: 800,
+    flipEffect: {
+        rotate: 0,
+        stretch: -10,
+        depth: 100,
+        modifier: 3,
+        slideShadows: !1
+    },
+    spaceBetween: 20,
+    autoplay: {
+            delay: 4000,
+            disableOnInteraction: !1
+        },
     loop: true,
     mousewheel: {
         invert: true,
     },
-    pagination: {
-        el: '.first-peg',
-        clickable:true,
-    },
     breakpoints: {
-        450: {
-            direction: 'vertical',
+        1200: {
+            slidesPerView: 2,
         },
-     },
+        992: {
+            slidesPerView: 1.8
+        },
+        570: {
+            slidesPerView: 1.7
+        },
+        480: {
+            slidesPerView: 1.5
+        }
+    }
 });
+
+
 
 const swiper1 = new Swiper('.secondSwiper', {
     direction: 'horizontal',
@@ -104,7 +163,7 @@ swiper1.on('transitionStart', function(e) {
         });
 
 
-
+     
 
 
 
