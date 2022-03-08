1263b4c1a6e66d976a08973feb990e0ff73e055f
Onur Altuntaş <50622451+Onurlulardan@users.noreply.github.com>
2022-03-08 09:57:45 +0300

    Add files via upload

--- miadent/master.js
+++ miadent/master.js
@@ -136,34 +136,104 @@ var swiper = new Swiper(".mysecondswiper", {
     },
   }
   });
-  // modal menu
-  const hmbgr = document.getElementById("hamburger");
-  const modal = document.querySelector(".modal-menu");
-  const closebtn = document.getElementById("modal-kapat");
-  const section = document.getElementById("section-cover");
 
-
-  hmbgr.addEventListener("click", ()=> {
-    modal.style.display = "flex";
-    section.style.display = "none";
+  var swiper = new Swiper('.tedaviSwiper', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    cssMode: true,
+    clickable: true,
+    loop: true,
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 40,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    }
   });
 
-  closebtn.addEventListener("click", ()=> {
-    modal.style.display = "none";
-    section.style.display = "block";
+  // // modal menu
+  // const hmbgr = document.getElementById("hamburger");
+  // const modal = document.querySelector(".modal-menu");
+  // const closebtn = document.getElementById("modal-kapat");
+  // const section = document.getElementById("section-cover");
+
+
+  // hmbgr.addEventListener("click", ()=> {
+  //   modal.style.display = "flex";
+  //   section.style.display = "none";
+  // });
+
+  // closebtn.addEventListener("click", ()=> {
+  //   modal.style.display = "none";
+  //   section.style.display = "block";
+  // });
+
+  
+  $("#hamburger").on( "click", function() {
+    $(".modal-menu").css("display", "flex");
+    $("#section-cover").css("display", "none");
   });
+ 
+$("#modal-kapat").on("click", function(){
+    $(".modal-menu").css("display", "none");
+    $("#section-cover").css("display", "block");
+});
 
   // randevu modal
-  const randevualbtn = document.getElementById("rndv-btn");
-  const randevu = document.querySelector(".randevu-modal");
-  const randevukapat = document.getElementById("randevu-kapat");
+  // const randevualbtn = document.getElementById("rndv-btn");
+  // const randevu = document.querySelector(".randevu-modal");
+  // const randevukapat = document.getElementById("randevu-kapat");
 
-  randevualbtn.addEventListener("click", ()=>{
-    randevu.style.display= "flex";
-    section.style.display = "none";
-  });
+  // randevualbtn.addEventListener("click", ()=>{
+  //   randevu.style.display= "flex";
+  //   section.style.display = "none";
+  // });
 
-  randevukapat.addEventListener("click", ()=>{
-    randevu.style.display= "none";
-    section.style.display = "block";
+  // randevukapat.addEventListener("click", ()=>{
+  //   randevu.style.display= "none";
+  //   section.style.display = "block";
+  // });
+
+  $("#rndv-btn").on( "click", function() {
+    $(".randevu-modal").css("display", "flex");
+    $("#section-cover").css("display", "none");
   });
+ 
+$("#randevu-kapat").on("click", function(){
+    $(".randevu-modal").css("display", "none");
+    $("#section-cover").css("display", "block");
+});
+
+
+  $(document).ready(function() {
+    $(".acc-content > a").on("click", function() {
+      if ($(this).hasClass("active")) {
+        $(this).removeClass("active");
+        $(this)
+          .siblings(".acc-inner-content")
+          .slideUp(200);
+        $(".acc-content  > a i")
+          .removeClass("fa-minus")
+          .addClass("fa-plus");
+      } else {
+        $(".acc-content > a i")
+          .removeClass("fa-minus")
+          .addClass("fa-plus");
+        $(this)
+          .find("i")
+          .removeClass("fa-plus")
+          .addClass("fa-minus");
+        $(".acc-content  > a").removeClass("active");
+        $(this).addClass("active");
+        $(".acc-inner-content").slideUp(200);
+        $(this)
+          .siblings(".acc-inner-content")
+          .slideDown(200);
+      }
+    });
+  });
+  
